98c2b62724b6ff5acd21e391c9ca5b2f6c88d7e6
Rheydskey <matgag02@gmail.com>
2022-10-11 23:32:53 +0200

    Remove useless things

--- pronote/lib/crypto.dart
+++ pronote/lib/crypto.dart
@@ -1,22 +1,7 @@
 import 'dart:typed_data';
 
-import 'package:crypton/crypton.dart';
 import 'package:pronote/crypto/aes.dart';
 import 'package:pronote/crypto/rsa.dart';
-
-class RsaKey {
-  BigInt modulus;
-  BigInt exponent;
-
-  RsaKey({required this.exponent, required this.modulus});
-
-  RSAPublicKey getKey() {
-    return RSAPublicKey(modulus, exponent);
-  }
-
-  static RsaKey fromString(String exponent, String modulus) => RsaKey(
-      exponent: BigInt.parse(exponent), modulus: BigInt.parse("0x$modulus"));
-}
 
 class Cipher {
   Rsa rsa;
